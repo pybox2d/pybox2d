caea573ea9c5908f23ba78865c00fcd0a312f956
sirkne <sirkne@bc78602e-eb4b-0410-95f1-012f1fce2112>
2011-02-26 17:38:15 +0000

    Moving all documentation out of trunk

--- doc/doxygen/generate.cmd
+++
@@ -1,6 +0,0 @@
-rem set DOXYGEN_PATH=c:\mingw\doxygen\bin
-set DOXYGEN_PATH=d:\dev\doxygen\bin
-set PYTHON_PATH=c:\python26
-
-%DOXYGEN_PATH%/doxygen.exe
-%PYTHON_PATH%/python.exe doxy2swig.py xml_output/index.xml ../../Box2D/Box2D_doxygen.i
